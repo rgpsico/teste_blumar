b71f13737ad351e7dd472502784f35d24e928b7e
roger neves <gerro121@hotmail.com>
2025-09-25 14:33:17 -0300

    index html

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
 import path from "path";
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        laravel({
+            input: ["resources/js/app.js"],
+            refresh: true, // habilita hot reload
+        }),
+        vue(),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "resources/js"),
